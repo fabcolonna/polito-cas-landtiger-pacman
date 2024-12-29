b8675ca7399ed0a998febafe18da06fef73365cf
Fabio Colonna <fabcolonna@icloud.com>
2024-12-29 10:10:16 +0100

    added base pacman stuff (does not compile for some reason)

---
+++ keil/Source/Program/PacMan/pacman.h
@@ -0,0 +1,6 @@
+#ifndef __PACMAN_H
+#define __PACMAN_H
+
+void PACMAN_Init(void);
+
+#endif
--- keil/Source/Program/main.c
+++ keil/Source/Program/main.c
@@ -1,8 +1,13 @@
 #include "includes.h"
+#include "pacman.h"
 
 int main(void)
 {
     SystemInit();
+    LCD_Init();
+
+    // Initialize the PacMan game
+    PACMAN_Init();
 
     POWER_Init(POWR_CFG_SLEEP_ON_EXIT);
     POWER_PowerDownOnWFI();
